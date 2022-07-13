10fa24ad952ed9a1710b5bd69a08e6e6d65ef4b6
yshaia7 <yshaia7@gmail.com>
2022-07-13 18:07:27 +0300

    JpaRepository: add Country and State Http Get request support
    Classes Added:
    - Country
    - State
    
    Interfaces Added:
    CountryRepository
    StateRepository

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/SpringBootEcommerceApplication.java
@@ -3,6 +3,15 @@ package com.luv2code.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/*
+* For add new entity support do the following:
+* 	1. create new class name with @Entity & @Table & @Setter & @Getter
+*	2. create new class interface that extends JpaRepository with @RepositoryRestResource & @CrossOrigin
+* 	3.
+*
+*
+* */
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
 
